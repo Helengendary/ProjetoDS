b3e88d30032cc8515fea7aefd77740092995f3fe
Helengendary <helenapicinin13@gmail.com>
2025-02-14 17:18:09 -0300

    Começo do front

--- src/routes/routes.ts
+++
@@ -1,9 +0,0 @@
-import { Express } from 'express';
-import express from 'express'
-import task from './task.ts'
-
-export default function (app: Express) {
-app
-    .use(express.json())
-    .use('/tasks', task)
-}
